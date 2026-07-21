ea7f075b92a1e67ab68a0b50d3bdfff263e2f54e
Mohamed-dev1 <mohamedsilent8@gmail.com>
2026-07-21 19:41:54 +0300

    submited Exercise 3

---
+++ Exercise 3/script.js
@@ -0,0 +1,16 @@
+// Exercise
+
+let isSuny = true;
+let isWekend = false;
+
+//and  &&
+console.log(isSuny && isWekend);// lawadoda isku mid ma ahan sidas daraded waa false
+
+// ||
+console.log(isSuny || isWekend); // naxaris aa ufirineyna mid kamid eh aa sax eh  true
+
+// !
+
+console.log(!isSuny); // waa cagsi hade sax ehed wuxu u badala qalad  jwbta wa false
+
+
