0a63493c231fe889f4344e7b6c787ae0f560d494
Mohamed-dev1 <mohamedsilent8@gmail.com>
2026-07-22 00:48:23 +0300

    submited

---
+++ Exercise 4/script.js
@@ -0,0 +1,20 @@
+//Exercise 4
+
+// 1
+function add(a, b) {
+  return a + b;
+}
+let jawaab = add(10, 20);
+console.log(jawaab);
+console.log(jawaab / 2);
+console.log(jawaab * 2);
+
+console.log("....");
+
+// 2  function expresions
+let iskuDar = function (m, n) {
+  return m + n;
+};
+
+let answer = iskuDar(10, 10);
+console.log(answer);
