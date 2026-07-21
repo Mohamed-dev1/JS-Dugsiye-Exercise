633bded33f57489fa873cebd89a7d30d8bd56720
Mohamed-dev1 <mohamedsilent8@gmail.com>
2026-07-22 01:22:55 +0300

    submited Exercise 5

---
+++ Exercise 5/script.js
@@ -0,0 +1,19 @@
+//Exercise 5
+
+//arrowfunction
+let Add = (a, b) => {
+  return a + b;
+};
+
+let jawaab = Add(10, 20);
+console.log(jawaab);
+
+// or i can use console
+//this one is not part of the exercie so did it like paractice
+let magac = (name) => {
+  console.log("soo Dhawaw: " + name);
+};
+
+magac("MR Mohamed ");
+magac("MR Halima ");
+magac("MR Abdi ");
