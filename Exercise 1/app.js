b27be6e45e1960013f50c3c82d2a66858b48b9c0
Mohamed-dev1 <mohamedsilent8@gmail.com>
2026-07-21 17:41:27 +0300

    sumbied Exercise 1

---
+++ Exercise 1/app.js
@@ -0,0 +1,11 @@
+//Exercise 1
+let x = 7;
+y = 3;
+
+console.log( x + y + " isku darka");
+console.log( x - y + " kala jarr");
+console.log( x * y + " isku dhufo");
+console.log( x / y + " isku qeebi");
+console.log( x % y + " isla ikaansho");
+console.log( ++x + "   hal ku dar");
+console.log( -- y + "  hal kajar");
