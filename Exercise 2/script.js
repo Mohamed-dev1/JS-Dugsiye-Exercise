3d31c4fb9d004c7abec72eeafa8db6216a6c7db5
Mohamed-dev1 <mohamedsilent8@gmail.com>
2026-07-21 18:55:11 +0300

    submited Exercise 2

---
+++ Exercise 2/script.js
@@ -0,0 +1,25 @@
+//Exercise 2
+
+let a = 15;
+let b = "15";
+
+//ma isla egyihin
+console.log(a == b);//jawabta true
+
+// isku mid ma ahn
+console.log(a != b);//jawabta false
+
+//si adag u fiiri dhinac walno
+console.log(a === b);//jawabta false
+
+//sifican u fiiri isku mi ma ahn
+console.log(a !== b); //jawabta true
+
+// greater than
+console.log(a > b);// jawabta false 
+ // lessthan
+console.log(a < b); // jawabta false
+//grater than or equal
+console.log(a >= b); // jawabta true
+// lessthan or equal
+console.log(a <= b); // jawabta true
