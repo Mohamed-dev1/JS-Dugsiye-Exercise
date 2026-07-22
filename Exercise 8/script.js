f419d27de4db79ef7a109f2865f4fbd8d27eba01
Mohamed-dev1 <mohamedsilent8@gmail.com>
2026-07-22 18:58:55 +0300

    submited Exercise 8

---
+++ Exercise 8/script.js
@@ -0,0 +1,12 @@
+// Exercise 8
+
+//objects
+let car = {
+  name: "toyota corolla",
+  model: "Gli",
+  year: 2022,
+};
+
+console.log("Magaca gaariga: " + car.name);
+console.log("Modelka: " + car.model);
+console.log("Sanadka: " + car.year);
