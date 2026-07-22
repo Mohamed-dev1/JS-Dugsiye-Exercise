b6f5f679a3e3a5508390f7dc3830e213b662b035
Mohamed-dev1 <mohamedsilent8@gmail.com>
2026-07-22 18:15:49 +0300

    submited Exercise 7

---
+++ Exercise 7/script.js
@@ -0,0 +1,15 @@
+//Exercise 7
+
+//Arrey methods
+
+let color = ["blue", "yellow", "red", "green"];
+
+// add a color to the enf of the colors arrey
+
+color.push("white");
+console.log(color);
+
+//remove the first volor
+
+color.shift();
+console.log(color);
