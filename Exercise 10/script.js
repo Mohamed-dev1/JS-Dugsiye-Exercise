654218eb73d1500957e4d43921ce73ed39e086d0
Mohamed-dev1 <mohamedsilent8@gmail.com>
2026-07-22 20:42:47 +0300

    submited Exercise 10

---
+++ Exercise 10/script.js
@@ -0,0 +1,18 @@
+//Exercise 10
+
+// arreys of object
+let Libary = [
+  {
+    Title: "it is not easy but through paractice and paractice",
+    Author: "mohamed",
+    Year: 2026,
+  },
+  {
+    title: "Dalka hormarkisa wa dhalinyarda",
+    author: "jamac",
+    year: 2026,
+  },
+];
+
+console.log(Libary[0].Title);
+console.log(Libary[1].author);
