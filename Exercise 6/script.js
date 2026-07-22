21302d74df0d82fdcfd7e57227b69413e72686e0
Mohamed-dev1 <mohamedsilent8@gmail.com>
2026-07-22 17:45:03 +0300

    submited Exercise 6

---
+++ Exercise 6/script.js
@@ -0,0 +1,12 @@
+//Exercise 6
+
+//Arrey
+
+let colors = ["bule", "red", "yellow"];
+
+// All
+console.log(colors);
+// one by one
+console.log(colors[0]);
+console.log(colors[1]);
+console.log(colors[2]);
