9bbe211a1bca445913312e55bff852e36677e959
Mohamed-dev1 <mohamedsilent8@gmail.com>
2026-07-25 04:07:11 +0300

    Submited Exercise 17

---
+++ Exercise 17/script.js
@@ -0,0 +1,15 @@
+//Exercise 17
+
+//else if
+
+let tempreture = 14; // jawabta very cold
+
+if (tempreture < 0) {
+  console.log("very cold");
+} else if (tempreture < 15) {
+  console.log("cold");
+} else if (tempreture < 25) {
+  console.lo("warm");
+} else {
+  console.log("hot");
+}
