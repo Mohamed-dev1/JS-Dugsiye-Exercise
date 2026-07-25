516d719cffe00df2b13c9d7863f412e683c0c5da
Mohamed-dev1 <mohamedsilent8@gmail.com>
2026-07-25 16:05:45 +0300

    Submited Exercise 20

---
+++ Exercise 20/script.js
@@ -0,0 +1,8 @@
+//Exercise 20
+// map
+let fruits = ["apple", "banana", "mango"];
+
+let jawaab = fruits.map((fruits) => fruits.length);
+console.log(jawaab);
+
+//.length waxuu shegaa ilaa iyo inta shey oo ka koobanyhey
