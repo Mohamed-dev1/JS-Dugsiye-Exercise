8422832875ab00abc27f6b1c8ebb9ad3d0205be3
Mohamed-dev1 <mohamedsilent8@gmail.com>
2026-07-25 03:59:39 +0300

    submited Exercise 16

---
+++ Exercise 16/script.js
@@ -0,0 +1,11 @@
+//Exercise 16
+
+//if else
+
+let score = 80;
+
+if (score > 50) {
+  console.log("you passed my friend'😎");
+} else {
+  console.log("you falied my friend i wish you the best next time");
+}
