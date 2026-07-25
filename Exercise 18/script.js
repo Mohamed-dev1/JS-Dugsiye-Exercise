2c86a6d6ccedb336c7b1147297fea717385d9be6
Mohamed-dev1 <mohamedsilent8@gmail.com>
2026-07-25 04:28:50 +0300

    Submited Exercise 18

---
+++ Exercise 18/script.js
@@ -0,0 +1,15 @@
+// Exercise 18
+
+//  ternary operator
+
+let grade = 50; //jawabta failed
+
+const jawab = grade >= 60 ? "passed" : "failed";
+
+console.log(jawab);
+
+//. waxa isku dayaa inaa ku daro &&
+
+const and = grade >= 60 && "failed"; // wuxo soo dabaca false
+
+console.log(and);
