1df901301cba4cfb1165699768ec6c9cc1ee42c0
Mohamed-dev1 <mohamedsilent8@gmail.com>
2026-07-26 17:56:58 +0300

    Submited Exercise 22

---
+++ Exercise 22/script.js
@@ -0,0 +1,9 @@
+//Exercise 22
+//Arrey destructing
+
+let color = ["red", "green", "blue"];
+const [red, green, blue] = color;
+
+//kala sobax elementa kowaad iyo lawaad
+console.log(red);
+console.log(blue);
