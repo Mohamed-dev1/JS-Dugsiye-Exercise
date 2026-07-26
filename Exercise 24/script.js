87593c568751dfe4931c07c0759389197f51e4b4
Mohamed-dev1 <mohamedsilent8@gmail.com>
2026-07-26 18:18:08 +0300

    Submited Exercise 24

---
+++ Exercise 24/script.js
@@ -0,0 +1,8 @@
+//Exercise 24
+//Defualt parameting
+function calculateArea(width, height = 5) {
+  console.log(`${(width, height)}`);
+}
+
+calculateArea();
+//midaan waan ku yara wareray but iga hubi 🏃‍♂️
