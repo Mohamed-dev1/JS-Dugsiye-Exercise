fc511b8e2200afd5f6be9f2f0925a8b1da693953
Mohamed-dev1 <mohamedsilent8@gmail.com>
2026-07-26 18:06:08 +0300

    Submited Exercise 23

---
+++ Exercise 23/script.js
@@ -0,0 +1,10 @@
+//Exercise 23
+
+//object destructing
+
+let cars = { make: "toyota", model: "corolla", year: 2020 };
+
+const { make, model, year } = cars;
+//lasoo bax make iyo model
+console.log(make);
+console.log(model);
