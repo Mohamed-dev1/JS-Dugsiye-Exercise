763404aa9097d37a0b15f10b8754a01047d11567
Mohamed-dev1 <mohamedsilent8@gmail.com>
2026-08-03 17:36:15 +0300

    Submited Exrcise 33

---
+++ Exercise 33/script.js
@@ -0,0 +1,8 @@
+//Exercise 33
+//inner html //textContent
+let jawb = document.getElementById(`hello`);
+console.log(jawb);
+
+function submit() {
+  hello.innerHTML = `soo dhawaw <srtong> Mohamed </ strong>`;
+}
